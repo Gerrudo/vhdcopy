235416a0c90728346bc6ec3922446ca58a9a8c4f
Tom <43029469+Gerrudo@users.noreply.github.com>
2022-02-17 23:36:00 +0000

    Updated Your'are mother balls
    because i'm the big epic coder round here sunshine

--- Hyper-VCopy.ps1
+++ Hyper-VCopy.ps1
@@ -1,53 +1,31 @@
-﻿$CurrentDate = get-date -format "dd/MM/yyyy HH:mm:ss"
-
-$CurrentDateLogs = get-date -format "yyyy/MM/dd"
-
-$path = "C:\BackupLogs\$CurrentDateLogs+'BackupLogs'.txt"
-
-Start-Transcript -Path $path -Force
-
-$sourceRoot = "D:\HyperV\VHDs"
-$destinationRoot = "\\Poweredge\f\Hyper-V Copy\VHDs"
-
-Copy-Item -Path $sourceRoot -Recurse -Destination $destinationRoot -Container -Force -Verbose
-
-echo "All Files Copied to Hyper-V Copy"
-
-Stop-Transcript
-
-$CurrentDateMsg = get-date -format "dd/MM/yyyy HH:mm:ss"
-
-# Embed with title, description, and color
-
-#Store webhook url
-$webHookUrl = 'https://discordapp.com/api/webhooks/498468005072863233/RQLROSwcRnrrRzNDyLKPM6FYhSIuWOiKNr9t7qw-aHD7ZJGK5YhktDWHMs5_JW1bPI5r'
-
-#Create embed array
-[System.Collections.ArrayList]$embedArray = @()
-
-#Store embed values
-$title       = $env:COMPUTERNAME
-$description = 'VHD backup completed at: ' + $CurrentDateMsg 
-$color       = '3722357'
-
-#Create embed object
-$embedObject = [PSCustomObject]@{
-
-    title       = $title
-    description = $description
-    color       = $color
-
+function Start-Backup{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $source,
+        [Parameter()]
+        [string]
+        $destination,
+        [Parameter()]
+        [string]
+        $discordWebhookUri
+    )
+    try {
+        Copy-Item -Path $source -Recurse -Destination $destination -Container -Force
+        $payload = [PSCustomObject]@{
+            $title       = $env:COMPUTERNAME
+            $description = "Backup complete"
+            $color       = "3722357"
+        }
+    }
+    catch {
+        Write-Error "Copy Failed with the following error: $($PSItem.Exception.Message)"
+        $payload = [PSCustomObject]@{
+            $title       = $env:COMPUTERNAME
+            $description = "Backup failed: $($PSItem.Exception.Message)"
+            $color       = "3722357" #should make this red that would be epic!!!!!!!! xD reddit poggers heccin doggo moment
+        }
+    }
+    Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json) -Method Post -ContentType 'application/json'
 }
-
-#Add embed object to array
-$embedArray.Add($embedObject) | Out-Null
-
-#Create the payload
-$payload = [PSCustomObject]@{
-
-    embeds = $embedArray
-
-}
-
-#Send over payload, converting it to JSON
-Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
